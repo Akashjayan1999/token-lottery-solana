1fbe6face23ddcab4851e50ca07a280132d8ae8f
Akashjayan1999 <akashjayan999@gmail.com>
2025-11-16 06:15:12 +0000

    add initialize lottery instruction

--- anchor/programs/token_lottery/src/lib.rs
+++ anchor/programs/token_lottery/src/lib.rs
@@ -1,10 +1,40 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{associated_token::AssociatedToken, metadata::Metadata, token_interface::{Mint, TokenAccount, TokenInterface}};
+use anchor_spl::{associated_token::AssociatedToken, token_interface::{Mint, TokenAccount, TokenInterface,mint_to,MintTo}};
 
+use anchor_spl::metadata::{
+    Metadata,
+    MetadataAccount,
+    CreateMetadataAccountsV3,
+    CreateMasterEditionV3,
+    SignMetadata,
+    SetAndVerifySizedCollectionItem,
+    create_master_edition_v3,
+    create_metadata_accounts_v3,
+    sign_metadata,
+    set_and_verify_sized_collection_item,
+    mpl_token_metadata::types::{
+            CollectionDetails,
+            Creator, 
+            DataV2,
+        },
+};
 declare_id!("AC6WCANoRqrqLnNmBX6mryScTzN1D5PAkbYuBCZR9x4F");
+
+#[constant]
+pub const NAME: &str = "Token Lottery Ticket #";
+#[constant]
+pub const URI: &str = "Token Lottery";
+#[constant]
+pub const SYMBOL: &str = "TICKET";
 
 #[program]
 pub mod token_lottery {
+   
+
+    
+
+   
+
     use super::*;
 
     pub fn initialize_config(ctx: Context<InitializeConifg>, start: u64, end: u64, price: u64) -> Result<()> {
@@ -21,7 +51,91 @@ pub mod token_lottery {
     }
 
     pub fn initilize_lottery(ctx: Context<InitilizeLottery>) -> Result<()> {
-        
+         
+         // Create Collection Mint
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"collection_mint".as_ref(),
+            &[ctx.bumps.collection_mint],
+        ]];
+
+        msg!("Creating collection mint");
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    mint: ctx.accounts.collection_mint.to_account_info(),
+                    to: ctx.accounts.collection_token_account.to_account_info(),
+                    authority: ctx.accounts.collection_mint.to_account_info(),
+                },
+                signer_seeds,
+            ),
+            1,
+        )?;
+
+         msg!("Creating metadata accounts");
+        create_metadata_accounts_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    metadata: ctx.accounts.metadata.to_account_info(),
+                    mint: ctx.accounts.collection_mint.to_account_info(),
+                    mint_authority: ctx.accounts.collection_mint.to_account_info(), // use pda mint address as mint authority
+                    update_authority: ctx.accounts.collection_mint.to_account_info(), // use pda mint as update authority
+                    payer: ctx.accounts.payer.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+                &signer_seeds,
+            ),
+            DataV2 {
+                name: NAME.to_string(),
+                symbol: SYMBOL.to_string(),
+                uri: URI.to_string(),
+                seller_fee_basis_points: 0,
+                creators: Some(vec![Creator {
+                    address: ctx.accounts.collection_mint.key(),
+                    verified: false,
+                    share: 100,
+                }]),
+                collection: None,
+                uses: None,
+            },
+            true,
+            true,
+            Some(CollectionDetails::V1 { size: 0 }), // set as collection nft
+        )?;
+
+         msg!("Creating Master edition accounts");
+        create_master_edition_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMasterEditionV3 {
+                    payer: ctx.accounts.payer.to_account_info(),
+                    mint: ctx.accounts.collection_mint.to_account_info(),
+                    edition: ctx.accounts.master_edition.to_account_info(),
+                    mint_authority: ctx.accounts.collection_mint.to_account_info(),
+                    update_authority: ctx.accounts.collection_mint.to_account_info(),
+                    metadata: ctx.accounts.metadata.to_account_info(),
+                    token_program: ctx.accounts.token_program.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+                &signer_seeds,
+            ),
+            Some(0),
+        )?;
+
+        msg!("verifying collection");
+        sign_metadata(CpiContext::new_with_signer(
+            ctx.accounts.token_metadata_program.to_account_info(),
+            SignMetadata {
+                creator: ctx.accounts.collection_mint.to_account_info(),
+                metadata: ctx.accounts.metadata.to_account_info(),
+            },
+            &signer_seeds,
+        ))?;
+
+
         Ok(())
     }
 }
